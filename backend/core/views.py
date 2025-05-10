dd6394b13652bd8edfed684b5fe70785e81aaf26
jason <j199180305@gmail.com>
2025-05-10 17:30:16 +0800

    Add home page

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import JsonResponse
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
