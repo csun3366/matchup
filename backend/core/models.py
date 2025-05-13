9d9a692f40432d87e129f1a8a2f9f8f3f247f045
jason <j199180305@gmail.com>
2025-05-13 21:08:46 +0800

    Add login mechanism and add input.html

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    username = models.CharField(max_length=50)
+    self_ig = models.CharField(max_length=50, blank=True, null=True)
+    other_ig = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,7 +1,37 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
-from django.http import JsonResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .models import Member
 
 def home(request):
     return render(request, 'home.html')
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You have been logged in!"))
+            return redirect('home')
+        else:
+            messages.success(request, ("帳號或密碼錯誤!"))
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You have been logged out!"))
+    return redirect('home')
+
+
+def register_user(request):
+    pass
+
+def input(request):
+    if request.method == "POST":
+        pass
+    else:
+        return render(request, 'input.html')
