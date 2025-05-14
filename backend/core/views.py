89348bcc2e038fefe05ec63cf07b54ae0f7ac91f
jason <j199180305@gmail.com>
2025-05-14 22:46:40 +0800

    Template out the base.html and navbar.html

--- backend/core/views.py
+++ backend/core/views.py
@@ -13,7 +13,6 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            messages.success(request, ("You have been logged in!"))
             return redirect('home')
         else:
             messages.success(request, ("帳號或密碼錯誤!"))
@@ -23,7 +22,6 @@ def login_user(request):
 
 def logout_user(request):
     logout(request)
-    messages.success(request, ("You have been logged out!"))
     return redirect('home')
 
 
