98e1e5e5d793c2abbac365fdadc2513aea56b826
jason <j199180305@gmail.com>
2025-05-15 23:14:45 +0800

    Add register page

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from .models import Member
 
 def home(request):
@@ -26,7 +27,24 @@ def logout_user(request):
 
 
 def register_user(request):
-    pass
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()       # 建立 User 並加密密碼
+            login(request, user)     # 可選，自動登入
+            return redirect('home')  # 或任何成功頁面
+        else:
+            # 加入欄位錯誤訊息到 messages
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f"{field}: {error}")
+
+            # 或也可以加入非欄位錯誤
+            for error in form.non_field_errors():
+                messages.error(request, error)
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
 
 def input(request):
     if request.method == "POST":
