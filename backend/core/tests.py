9062d116cebade3294c2ac9004f34ccebe1b8433
jason <j199180305@gmail.com>
2025-05-19 23:31:23 +0800

    Add login unit test

--- backend/core/tests.py
+++ backend/core/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+class SimpleTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.username = "testuser"
+        self.password = "testpass123"
+        self.user = User.objects.create_user(username=self.username, password=self.password)
+    def test_login_success(self):
+        response = self.client.post(reverse('login'), {
+            'username': self.username,
+            'password': self.password
+        })
+        self.assertEqual(response.status_code, 302)  # 通常登入成功會 redirect
+        self.assertTrue(response.wsgi_request.user.is_authenticated)
+    def test_login_failure(self):
+        response = self.client.post(reverse('login'), {
+            'username': self.username,
+            'password': 'wrongpass'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(response.wsgi_request.user.is_authenticated)
